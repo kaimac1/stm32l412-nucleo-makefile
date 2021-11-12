4ffe73b60faf6c24a8e34fd4243a4fa12ec13198
Kyle McInnes <kylesrmcinnes@gmail.com>
2021-11-12 21:36:34 +0000

    Working USART TX

--- src/hardware/nucleo.c
+++ src/hardware/nucleo.c
@@ -22,31 +22,30 @@ void nucleo_led_init(void) {
 
 void nucleo_uart_init(void) {
 
-    // __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
 
-    // // UART TX pin
-    // LL_GPIO_InitTypeDef gpio;
-    // gpio.Pin = LL_GPIO_PIN_2;
-    // gpio.Mode = LL_GPIO_MODE_ALTERNATE;
-    // gpio.Speed = LL_GPIO_SPEED_HIGH;
-    // gpio.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-    // gpio.Pull = 0;    
-    // gpio.Alternate = LL_GPIO_AF_7;
-    // LL_GPIO_Init(GPIOA, &gpio);
+    // UART TX pin
+    LL_GPIO_InitTypeDef gpio;
+    gpio.Pin = LL_GPIO_PIN_2;
+    gpio.Mode = LL_GPIO_MODE_ALTERNATE;
+    gpio.Speed = LL_GPIO_SPEED_HIGH;
+    gpio.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    gpio.Pull = 0;    
+    gpio.Alternate = LL_GPIO_AF_7;
+    LL_GPIO_Init(GPIOA, &gpio);
 
-    // __HAL_RCC_USART2_CLK_ENABLE();
-    // LL_USART_InitTypeDef usart;
-    // usart.PrescalerValue = LL_USART_PRESCALER_DIV4; // check
-    // usart.BaudRate = 115200;
-    // usart.DataWidth = LL_USART_DATAWIDTH_8B;
-    // usart.StopBits = LL_USART_STOPBITS_1;
-    // usart.Parity = LL_USART_PARITY_NONE;
-    // usart.TransferDirection = LL_USART_DIRECTION_TX_RX;
-    // usart.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
-    // usart.OverSampling = LL_USART_OVERSAMPLING_16; 
-    // LL_USART_Init(DEBUG_UART, &usart);
+    __HAL_RCC_USART2_CLK_ENABLE();
+    LL_USART_InitTypeDef usart;
+    usart.BaudRate = 115200;
+    usart.DataWidth = LL_USART_DATAWIDTH_8B;
+    usart.StopBits = LL_USART_STOPBITS_1;
+    usart.Parity = LL_USART_PARITY_NONE;
+    usart.TransferDirection = LL_USART_DIRECTION_TX_RX;
+    usart.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+    usart.OverSampling = LL_USART_OVERSAMPLING_16; 
+    LL_USART_Init(DEBUG_UART, &usart);
 
-    // LL_USART_Enable(DEBUG_UART);    
+    LL_USART_Enable(DEBUG_UART);    
 
 }
 
